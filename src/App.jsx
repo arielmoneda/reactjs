7ebcc2df8632e559cc6a34708de2f7616e7670b5
arielmoneda <ariel.moneda@gmail.com>
2024-12-04 05:06:02 -0800

    ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="w-screen h-screens bg-red-300 flex-col gap-5 ps-1">
+      <h1>Hello</h1>
+      <h2>World</h2>
+      <div className="w-screen h-screen bg-purple-50 flex justify-center items-center gap-5 ">
+        YELLOW
+      </div>
+    </div>
+  );
+}
+
+export default App;
